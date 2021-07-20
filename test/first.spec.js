172e6c3a94bcd82a4af7e84b9f0e7ed8a1d3cc29
raghunath.venkata <raghunath.venkata@capco.com>
2021-07-20 17:51:59 +0100

    sauce test

--- test/first.spec.js
+++ test/first.spec.js
@@ -1,10 +1,9 @@
 const { chromium, firefox, webkit } = require('playwright');
 
 (async () => {
-  const browser = await chromium.launch();  // Or 'firefox' or 'webkit'.
+  const browser = await chromium.launch({headless: false});
   const page = await browser.newPage();
   await page.goto('https://www.bbc.com');
   console.log(await page.title());
-  // other actions...
   await browser.close();
 })();
